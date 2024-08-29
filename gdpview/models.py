5cb2622d903f6d4cd30eba31c2a59eb01369849e
samoyed24 <1601824795@qq.com>
2024-08-29 16:17:34 +0800

    bug fixed

--- gdpview/models.py
+++ gdpview/models.py
@@ -8,14 +8,8 @@ class GDPdata(models.Model):
     value = models.FloatField(null=True, blank=True)
     type = models.TextField(null=False, blank=False)
 
-    # def __str__(self):
-    #     return f"{self.region_name}{self.year}年GDP：{self.value}亿元"
-
 
 class NationalData(models.Model):
     type = models.TextField(null=False, blank=False)
     value = models.FloatField(null=True, blank=True)
     year = models.TextField(null=False, blank=False)
-
-    # def __str__(self):
-    #     return f"我国{self.year}年{self.type}：{self.value}亿元/元"
